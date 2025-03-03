15c3b47d2b3160d2ff0c113e016605934766000f
stdiodh <playlistdh@gmail.com>
2025-03-03 13:37:01 +0900

    cicd 파일 수정, 필요없는 의존성 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,15 +23,11 @@ extra["springCloudVersion"] = "2024.0.0"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
 	implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.3.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
